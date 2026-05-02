1c3d84f31aeb54a258212f96940b3876489e9dff
taimoors-git <taimoormughal144329@gmail.com>
2026-05-02 21:35:01 +0500

    cards component

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Card from './components/card.jsx';
+import './App.css';
+import Navbar from './components/navbar.jsx'
+
 
 const App = () => {
   return (
+    <>
+    <div className="navigation">
+      <Navbar />
+    </div>
     <div className='parent'>
       <Card name = "Taimoor" age = {22} img="https://images.unsplash.com/photo-1774327988852-531c6eba5397?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
       <Card name = "Arshad" age = {70} img="https://images.unsplash.com/photo-1777221490079-b949ecac1a9e?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
       <Card name = "Mughal" age = {23} img="https://images.unsplash.com/photo-1770041797744-59e295885e8b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2M3x8fGVufDB8fHx8fA%3D%3D"/>
     </div>
+    </>
   )
 }
 
---
+++ first-react-project/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar(){
+    return (
+        <div className="bar">
+            <div className="logo">LOGO</div>
+            <div className="pages">
+                <div className="home">HOME</div>
+                <div className="about">ABOUT US</div>
+                <div className="contactus">CONTACT US</div>
+                <div className="projects">PROJECTS</div>
+            </div>
+
+            <div className="search">
+                <input type="text" />
+            </div>
+        </div>
+    )
+};
+export default Navbar
